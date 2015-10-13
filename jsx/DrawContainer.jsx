6d5853fd938162ba4fcb74a997561b0d6c8c8a7f
Joe <bazzooka@gmail.com>
2015-10-13 12:23:35 +0200

    hide and show curve handler with selection

--- jsx/DrawContainer.jsx
+++ jsx/DrawContainer.jsx
@@ -35,12 +35,12 @@ let DrawContainer = React.createClass({
 
     me.svgElt = this.refs.svgElt.getDOMNode();
 
-    me.svgElt.addEventListener("mousedown", (e) => {
-      if (e.srcElement.classList.contains("position-handler") || e.srcElement.classList.contains("curve-handler")) {
+    me.svgElt.addEventListener('mousedown', (e) => {
+      if (e.srcElement.classList.contains('position-handler') || e.srcElement.classList.contains('curve-handler')) {
         me.dragParams.isDragging = true;
         me.dragParams.dragElt = e.srcElement;
-        me.dragParams.dragIndex = e.srcElement.getAttribute("data-id");
-        me.dragParams.dragCurveIndex = e.srcElement.getAttribute("data-pos") || null;
+        me.dragParams.dragIndex = e.srcElement.getAttribute('data-id');
+        me.dragParams.dragCurveIndex = e.srcElement.getAttribute('data-pos') || null;
         me.dragParams.command = me.state.paths.controls[me.dragParams.dragIndex].command;
 
         // Create an SVGPoint for future math
@@ -53,26 +53,26 @@ let DrawContainer = React.createClass({
         me.dragParams.dragElt = null;
         me.dragParams.dragIndex = -1;
         me.dragParams.dragCurveIndex = -1;
-        me.dragParams.command = "";
+        me.dragParams.command = '';
       }
       this.updateHandlersColors(e);
     });
 
-    this.refs.svgElt.getDOMNode().addEventListener("mousemove", (e) => {
+    this.refs.svgElt.getDOMNode().addEventListener('mousemove', (e) => {
       if (me.dragParams.isDragging) {
         let mPos = this.getMousePositionInSVG(e);
         me.updatePosition(mPos.x, mPos.y);
       }
     });
 
-    this.refs.svgElt.getDOMNode().addEventListener("mouseup", (e) => {
+    this.refs.svgElt.getDOMNode().addEventListener('mouseup', (e) => {
       if (me.dragParams.isDragging) {
         me.dragParams.isDragging = false;
         me.dragParams.dragElt = null;
       }
     });
 
-    window.addEventListener("resize", (e) =>{
+    window.addEventListener('resize', (e) => {
       me.resize();
     })
 
@@ -80,25 +80,31 @@ let DrawContainer = React.createClass({
   },
 
   updateHandlersColors: function(e) {
-    // Remove all selected class from position-handlers
-    Array.prototype.forEach.call(this.refs.svgElt.getDOMNode().getElementsByClassName('position-handler'), function(elt) {
+    // Remove all selected class from position-handlers and overLapPath
+    let hiddenElements = ['.position-handler', '.overLapPath', '.curve-handler', '.path-to-origin'].join(',');
+    Array.prototype.forEach.call(this.refs.svgElt.getDOMNode().querySelectorAll(hiddenElements), function(elt) {
       elt.classList.remove('selected');
     });
 
-    // Desactive all overLapPath
-    Array.prototype.forEach.call(this.refs.svgElt.getDOMNode().getElementsByClassName('overLapPath'), function(elt) {
-      elt.classList.remove('selected');
-    });
+    if (e.srcElement.classList.contains('position-handler') && this.dragParams.isDragging) {
+      this.dragParams.dragElt.classList.add('selected');
 
-    if (e.srcElement.classList.contains("position-handler") && this.dragParams.isDragging) {
-      this.dragParams.dragElt.classList.add("selected");
-
-      if (this.dragParams.dragIndex !== "0") {
-        this.refs.svgElt.getDOMNode().getElementsByClassName('overLapPath-' + (this.dragParams.dragIndex))[0].classList.add("selected");
+      if (this.dragParams.dragIndex !== '0') {
+        // Active Overlap
+        this.refs.svgElt.getDOMNode().getElementsByClassName('overLapPath-' + (this.dragParams.dragIndex))[0].classList.add('selected');
       }
     }
 
+    if ((e.srcElement.classList.contains('position-handler') && this.dragParams.isDragging) ||(e.srcElement.classList.contains('curve-handler'))) {
+      let activeClasses = ['.path-to-origin-' + this.dragParams.dragIndex, '.curve-handler-' + this.dragParams.dragIndex].join(', ');
+      Array.prototype.forEach.call(this.refs.svgElt.getDOMNode().querySelectorAll(activeClasses), function(elt) {
+          elt.classList.add('selected');
+        });
+    }
+
+    // Select row in pathCommander
     this.props.onSelectHandler(this.dragParams.dragIndex);
+
   },
 
   updatePosition: function(x, y) {
@@ -106,26 +112,26 @@ let DrawContainer = React.createClass({
       newY = parseInt(y, 10);
 
     if (this.dragParams.dragCurveIndex) {
-      this.dragParams.dragElt.setAttribute("x", newX);
-      this.dragParams.dragElt.setAttribute("y", newY);
+      this.dragParams.dragElt.setAttribute('x', newX);
+      this.dragParams.dragElt.setAttribute('y', newY);
 
       let newPosition = {};
-      newPosition["x" + this.dragParams.dragCurveIndex] = newX;
-      newPosition["y" + this.dragParams.dragCurveIndex] = newY;
+      newPosition['x' + this.dragParams.dragCurveIndex] = newX;
+      newPosition['y' + this.dragParams.dragCurveIndex] = newY;
       newPosition.index = this.dragParams.dragIndex;
       newPosition.dragCurveIndex = this.dragParams.dragCurveIndex;
 
       this.props.onDrawChanged(newPosition);
 
     } else {
-      if (this.dragParams.command !== "V") {
-        this.dragParams.dragElt.setAttribute("x", newX - this.handlerParams.halfSize);
+      if (this.dragParams.command !== 'V') {
+        this.dragParams.dragElt.setAttribute('x', newX - this.handlerParams.halfSize);
       } else {
         newX = null;
       }
 
-      if (this.dragParams.command !== "H") {
-        this.dragParams.dragElt.setAttribute("y", newY - this.handlerParams.halfSize);
+      if (this.dragParams.command !== 'H') {
+        this.dragParams.dragElt.setAttribute('y', newY - this.handlerParams.halfSize);
       } else {
         newY = null;
       }
@@ -153,26 +159,26 @@ let DrawContainer = React.createClass({
 
     for (let i = 0, l = newPath.controls.length; i < l; i++) {
       let path = newPath.controls[i];
-      if (["M", "L", "H", "V", "C", "S", "Q", "T"].indexOf(path.command) !== -1) {
+      if (['M', 'L', 'H', 'V', 'C', 'S', 'Q', 'T'].indexOf(path.command) !== -1) {
         this.handlers.push({
-          type: "circle",
+          type: 'circle',
           index: i,
           x: parseInt(newPath.controls[i].x) - this.handlerParams.halfSize,
           y: parseInt(newPath.controls[i].y) - this.handlerParams.halfSize
         });
       }
-      if (["C", "Q"].indexOf(path.command) !== -1) {
+      if (['C', 'Q'].indexOf(path.command) !== -1) {
         this.curveHandlers.push({
-          type: "rect",
+          type: 'rect',
           index: i,
           pos: 1,
           x: parseInt(newPath.controls[i].x1),
           y: parseInt(newPath.controls[i].y1)
         });
       }
-      if (["C", "S"].indexOf(path.command) !== -1) {
+      if (['C', 'S'].indexOf(path.command) !== -1) {
         this.curveHandlers.push({
-          type: "rect",
+          type: 'rect',
           index: i,
           pos: 2,
           x: parseInt(newPath.controls[i].x2),
@@ -180,26 +186,25 @@ let DrawContainer = React.createClass({
         });
       }
 
-
       /*
        * Determine each segments of master path
        */
-       let last
+      let last
       switch (path.command) {
-        case "M": this.middlePaths.push({
+        case 'M': this.middlePaths.push({
             xStart: path.x,
             yStart: path.y,
             xEnd: path.x,
             yEnd: path.y,
-            command: "Z",
+            command: 'Z',
             index: i
           });
           break;
-        case "L":
-        case "T":
-        case "C":
-        case "S":
-        case "Q":
+        case 'L':
+        case 'T':
+        case 'C':
+        case 'S':
+        case 'Q':
           this.middlePaths[i - 1].x1 = path.x1 || null;
           this.middlePaths[i - 1].y1 = path.y1 || null;
           this.middlePaths[i - 1].x2 = path.x2 || null;
@@ -213,13 +218,13 @@ let DrawContainer = React.createClass({
             yStart: path.y,
             xEnd: path.x,
             yEnd: path.y,
-            command: "Z"
+            command: 'Z'
           });
 
           this.middlePaths[i - 1].command = path.command;
           this.middlePaths[i - 1].index = i;
           break;
-        case "H":
+        case 'H':
           this.middlePaths[i - 1].xEnd = path.x;
 
           this.middlePaths.push({
@@ -227,12 +232,12 @@ let DrawContainer = React.createClass({
             yStart: this.middlePaths[i - 1].yEnd,
             xEnd: path.x,
             yEnd: this.middlePaths[i - 1].yEnd,
-            command: "Z"
+            command: 'Z'
           });
           this.middlePaths[i - 1].command = path.command;
           this.middlePaths[i - 1].index = i;
           break;
-        case "V":
+        case 'V':
           this.middlePaths[i - 1].yEnd = path.y;
 
           this.middlePaths.push({
@@ -240,7 +245,7 @@ let DrawContainer = React.createClass({
             yStart: path.y,
             xEnd: this.middlePaths[i - 1].xEnd,
             yEnd: path.y,
-            command: "Z"
+            command: 'Z'
           });
           this.middlePaths[i - 1].command = path.command;
           this.middlePaths[i - 1].index = i;
@@ -264,9 +269,9 @@ let DrawContainer = React.createClass({
       curveHandlers = null,
       curvePointToOrigin = null,
       middlePaths = null,
-      defs = "";
+      defs = '';
     if (this.state.paths) {
-      paths = <path d={this.state.paths.path} className="blurred"></path>
+      paths = <path d={this.state.paths.path} className='blurred'></path>
     }
 
     /*
@@ -276,7 +281,7 @@ let DrawContainer = React.createClass({
       let oldEltX = null,
         oldEltY = null;
       positionHandlers = this.handlers.map((elt, i) => {
-        let posHand = <rect className="position-handler" x={elt.x || oldEltX.x} y={elt.y || oldEltY.y} width={this.handlerParams.cubeSize} height={this.handlerParams.cubeSize} data-id={elt.index} key={i}></rect>;
+        let posHand = <rect className='position-handler' x={elt.x || oldEltX.x} y={elt.y || oldEltY.y} width={this.handlerParams.cubeSize} height={this.handlerParams.cubeSize} data-id={elt.index} key={i}></rect>;
         oldEltX = elt.x ? elt : oldEltX;
         oldEltY = elt.y ? elt : oldEltY;
         return posHand;
@@ -288,7 +293,8 @@ let DrawContainer = React.createClass({
      */
     if (this.curveHandlers.length !== 0) {
       curveHandlers = this.curveHandlers.map((elt, i) => {
-        return <circle className="curve-handler" cx={elt.x} cy={elt.y} r={this.handlerParams.rayon} data-id={elt.index} data-pos={elt.pos} key={i}></circle>;
+        let classNames = 'curve-handler curve-handler-' + elt.index;
+        return <circle className={classNames} cx={elt.x} cy={elt.y} r={this.handlerParams.rayon} data-id={elt.index} data-pos={elt.pos} key={i}></circle>;
       });
 
       // Draw line between curve modifier and it's origin
@@ -304,67 +310,67 @@ let DrawContainer = React.createClass({
           xEnd = this.state.paths.controls[elt.index].x;
           yEnd = this.state.paths.controls[elt.index].y;
         }
-        let newPathToOrigin = ["M", xStart, yStart, "L", xEnd, yEnd].join(" ");
-        return <path d={newPathToOrigin} key={i}></path>
-        });
-      }
+        let newPathToOrigin = ['M', xStart, yStart, 'L', xEnd, yEnd].join(' '),
+          classNames = 'path-to-origin path-to-origin-' + elt.index;
+        return <path className={classNames} d={newPathToOrigin} key={i}></path>
+      });
+    }
 
-      /*
-       * Draw middlePaths
-       */
-      let middlePathsSize = this.middlePaths.length;
-      middlePaths = this.middlePaths.map(function(elt, i) {
+    /*
+     * Draw middlePaths
+     */
+    let middlePathsSize = this.middlePaths.length;
+    middlePaths = this.middlePaths.map(function(elt, i) {
         if (middlePathsSize - 1 === i) {
           return;
         }
-        let classIndex = "overLapPath overLapPath-" + elt.index,
-          path = ["M", elt.xStart, elt.yStart, elt.command].join(" ") + " ";
+        let classIndex = 'overLapPath overLapPath-' + elt.index,
+          path = ['M', elt.xStart, elt.yStart, elt.command].join(' ') + ' ';
 
-        path += (elt.x1 ? [elt.x1, elt.y1].join(" ") : "") + " ";
-        path += (elt.x2 ? [elt.x2, elt.y2].join(" ") : "") + " ";
+        path += (elt.x1 ? [elt.x1, elt.y1].join(' ') : '') + ' ';
+        path += (elt.x2 ? [elt.x2, elt.y2].join(' ') : '') + ' ';
 
-        path += (elt.xEnd && elt.command !== 'V')? [elt.xEnd, " "].join(" ") : " ";
-        path += (elt.yEnd && elt.command !== 'H')? [elt.yEnd, " "].join(" ") : " ";
+        path += (elt.xEnd && elt.command !== 'V') ? [elt.xEnd, ' '].join(' ') : ' ';
+        path += (elt.yEnd && elt.command !== 'H') ? [elt.yEnd, ' '].join(' ') : ' ';
 
         return (
           <path d={path} key={i} className={classIndex}></path>
           )
       });
 
+    /*
+     * Define SVG defs
+     */
 
-      /*
-       * Define SVG defs
-       */
+    defs = '<filter id="blurred" filterUnits="userSpaceOnUse" x="0" y="0" width="400" height="400">';
+    defs += '<feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>';
+    defs += '<feMerge>';
+    defs += '<feMergeNode in="coloredBlur"/>';
+    defs += '<feMergeNode in="SourceGraphic"/>';
+    defs += '</feMerge>';
+    defs += '</filter>';
 
-      defs = '<filter id="blurred" filterUnits="userSpaceOnUse" x="0" y="0" width="400" height="400">';
-      defs += '<feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>';
-      defs += '<feMerge>';
-      defs += '<feMergeNode in="coloredBlur"/>';
-      defs += '<feMergeNode in="SourceGraphic"/>';
-      defs += '</feMerge>';
-      defs += '</filter>';
+    let viewBox = [0, 0, this.svgParams.viewBox_w, this.svgParams.viewBox_h].join(' ');
 
-      let viewBox = [0, 0, this.svgParams.viewBox_w, this.svgParams.viewBox_h].join(" ");
-
-      return (
-        <div id="draw-container" className="draw-container">
-          <svg ref="svgElt" viewBox="0 0 80 80" width="100%" height="100%" fill="currentcolor" className="svg-style">
+    return (
+      <div id='draw-container' className='draw-container'>
+                  <svg ref='svgElt' viewBox='0 0 80 80' width='100%' height='100%' fill='currentcolor' className='svg-style'>
             <defs dangerouslySetInnerHTML={{
           __html: defs
         }}>
             </defs>
-            <rect x="0" y="0" width="100%" height="100%" fill="black"/>
-            <SVG_Grid ref="grid"/>
+            <rect x='0' y='0' width='100%' height='100%' fill='black'/>
+            <SVG_Grid ref='grid'/>
             {paths}
             {middlePaths}
             {curvePointToOrigin}
             {positionHandlers}
             {curveHandlers}
             
-          </svg>
+                                                                                                                                                                                                                                                                                                                                </svg>
         </div>
         )
-    }
-  });
+  }
+});
 
-  module.exports = DrawContainer;
+module.exports = DrawContainer;
